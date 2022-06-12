a693354aefbeab10de87a099831393ec80c27b59
Oleg Vaskevich <oleg.vaskevich.it@gmail.com>
2022-06-12 13:06:22 +0300

    feat: implement hash command

---
+++ src/cli/hash/hash.js
@@ -0,0 +1,20 @@
+import { createHash } from 'crypto';
+import { readFile } from 'fs/promises';
+import { currentDirMessage, getCurrentDir } from '../../helpers.js';
+
+export const hash = async ([ filePath ]) => {
+  try {
+    const fileBuffer = await readFile(filePath);
+    const hashSum = createHash('sha256');
+
+    hashSum.update(fileBuffer);
+    
+    const hex = hashSum.digest('hex');
+  
+    console.log(hex);
+  } catch {
+    console.log('Invalid input');
+  } finally {
+    currentDirMessage(getCurrentDir());
+  }
+}
--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ import { mv } from './cli/fs/mv.js';
 import { rm } from './cli/fs/rm.js';
 import { rn } from './cli/fs/rn.js';
 import { os } from './cli/os/os.js';
+import { hash } from './cli/hash/hash.js';
 
 
 const start = async () => {
@@ -80,6 +81,11 @@ const start = async () => {
     // os
     if (command.startsWith('os')) {
       os(getComandProps(command))
+    }
+
+    // hash
+    if (command.startsWith('hash')) {
+      hash(getComandProps(command))
     }
   });
 
